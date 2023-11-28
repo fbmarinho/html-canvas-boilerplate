92705b807574977ce45a63e117818b45a5c627a9
Felipe Marinho <felipegigamail@gmail.com>
2023-11-28 02:49:34 -0300

    Updade

--- src/script.js
+++ src/script.js
@@ -1,8 +1,13 @@
 const canvas = document.getElementsByClassName('canvas')[0];
 const ctx = canvas.getContext("2d");
 
+ctx.cls("black");
+ctx.strokeStyle = "blue";
+ctx.circle(WIDTH/2,HEIGHT/2,HEIGHT/2);
+
 const WIDTH = 0;
 const HEIGHT = 0;
+
 
 function resize(){
     WIDTH = document.body.clientWidth;
@@ -13,8 +18,17 @@ function resize(){
 
 resize();
 
-CanvasRenderingContext2D.prototype.cls = function(){
+
+CanvasRenderingContext2D.prototype.cls = function(color){
+    this.fillStyle = color;
     this.fillRect(0, 0, WIDTH, HEIGHT);
+}
+
+CanvasRenderingContext2D.prototype.circle = function(x,y,r){
+    this.beginPath();
+    this.arc(x, y, r, 0, 2 * Math.PI);
+    this.closePath();
+    this.stroke();
 }
 
 window.addEventListener("resize",()=>resize());
